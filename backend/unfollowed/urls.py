9d92000db39c698472b923ef2968f987fc7ebf7f
Raj Kamal Singh <rkssisodiya@gmail.com>
2015-02-01 01:55:08 +0530

    made empty url redrect to static/index.html

--- backend/unfollowed/urls.py
+++ backend/unfollowed/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url, static
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 urlpatterns = patterns('',
     # Examples:
@@ -8,7 +9,12 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^auth/', include('apps.authentication.urls')),
+    url(r'^auth/', include('apps.authentication.urls'))
 )
 # static files will be served at /
-#urlpatterns += static.static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static.static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+index_url = "/static/index.html"
+urlpatterns += patterns('',
+    url(r'^', RedirectView.as_view(url=index_url, permanent=False)),
+)
