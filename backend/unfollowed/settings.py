ba9d53310af745c4cfe73164dce81ec7992a19d9
Raj Kamal Singh <rkssisodiya@gmail.com>
2015-01-31 21:26:17 +0530

    Add social app to settings and clear migration

--- backend/unfollowed/settings.py
+++ backend/unfollowed/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
         'django.contrib.messages',
         'django.contrib.staticfiles',
         'apps.authentication',
+        'apps.social',
         'rest_framework',
         )
 
