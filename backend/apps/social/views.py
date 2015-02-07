b53b13fb0a177a372fcf545cfd32cd190653bd3c
Harshul Jain <harshulj@gmail.com>
2015-02-08 04:54:58 +0530

    Fixed statuscode error and added User id.

--- backend/apps/social/views.py
+++ backend/apps/social/views.py
@@ -6,19 +6,20 @@ from apps.authentication.models import SocialAccount
 @api_view(['GET', 'POST'])
 def current_user(request):
     if not request.user.is_authenticated():
-        error = {'message': 'User unauthenticated.', 'code': 401}
-        return Response({'errors': [error]}, status_code=403)
+        error = {'message': 'User unauthenticated.', 'code': 403}
+        return Response({'errors': [error]}, status=403)
 
     if request.method == 'GET':
         account = SocialAccount.objects.filter(user=request.user, account_type=SocialAccount.TWITTER)[0]
         profile = account.profile
         data = {}
+        data['id']          = request.user.id
         data['name']        = profile.name
         data['username']    = profile.username
         data['picture']     = profile.picture
         data['email']       = profile.email
         data['profiles']    = []
-        return Response({'_data': data})
+        return Response({'_data': data}, status=200)
 
     elif request.method == 'POST':
-        return Response({}, status_code=204)
+        return Response({}, status=204)
